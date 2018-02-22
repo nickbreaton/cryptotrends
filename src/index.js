265f87e83fed5bb31ef4cda135bba19ee2108620
nickbreaton <nick@breaton.com>
2018-02-22 00:19:43 -0500

    Add redux persist

--- src/index.js
+++ src/index.js
@@ -1,22 +1,30 @@
 import { createStore } from 'redux-box'
 import { module as app } from './state/app'
 import { render } from 'react-dom'
+import { persistStore, persistReducer } from 'redux-persist'
 import createWorkerMiddleware from 'redux-worker-middleware'
 import PointsWorker from './workers/points.worker'
 import React from 'react'
+import storage from 'redux-persist/lib/storage'
 
 const store = createStore([ app ], {
   middlewares: [
     createWorkerMiddleware(new PointsWorker)
-  ]
+  ],
+  decorateReducer: reducer => {
+    return persistReducer({ key: 'test', storage }, reducer);
+  }
 })
 
+persistStore(store)
+
+const Stringify = ({ value = {} }) => (
+  <pre>{JSON.stringify(value, null, 2)}</pre>
+)
+
 store.subscribe(() => {
-  const App = () => (
-    <pre>
-      {JSON.stringify(store.getState(), null, 2)}
-    </pre>
-  )
+  const { app: { points } } = store.getState()
+  const App = () => ( <Stringify value={points} /> )
   render(<App />, document.querySelector('#root'))
 })
 
