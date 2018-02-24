822dde687252459bf417d5ca5e25f82756931a83
nickbreaton <nick@breaton.com>
2018-02-24 14:49:22 -0500

    Move towards wanted colors

--- src/index.js
+++ src/index.js
@@ -40,60 +40,93 @@ start.setFullYear(start.getFullYear() - 1)
 
 // -------- Visualization Code -------
 import * as d3 from 'd3'
+import { complement } from 'polished'
+import debounce from 'lodash.debounce'
 
+const root = d3.select('#root')
+  .style('padding-top', '2rem')
+  .style('display', 'flex')
+  .style('align-items', 'center')
+  .style('justify-content', 'center')
 
-const svg = d3.select('#root').append('svg')
+const svg = root.append('svg')
 
-svg
-  .attr('width', 1200)
-  .attr('height', 400)
-  .style('background', '#2c4764')
-
-const width = +svg.attr('width')
-const height = +svg.attr('height')
-
-const x = d3.scaleTime().range([0, width])
-const y0 = d3.scaleLinear().range([height, 0])
-const y1 = d3.scaleLinear().range([height, 0])
-
-const priceLine = d3.area()
-  .curve(d3.curveCardinal)
-  .x(d => x(d.date))
-  .y1(d => y0(d.price))
-  .y0(y0(0))
-
-const interestLine = d3.area()
-  .curve(d3.curveCardinal)
-  .x(d => x(d.date))
-  .y1(d => y1(d.interest))
-  .y0(y1(0))
+window.addEventListener('resize', () => {
+  window.requestAnimationFrame(() => {
+    svg.node().innerHTML = ''
+    const { app: { points, isLoading } } = store.getState()
+    render(points)
+  })
+})
 
 function render(points) {
-  console.log(points.length)
+  svg
+    .attr('width', 900)
+    .attr('height', 400)
+    // .style('background', 'white')
+    // .style('border', '1px solid #ccc')
+    // .style('border-radius', '5px')
+
+  const padding = 45
+  const width = svg.node().getBoundingClientRect().width - padding
+  const height = svg.node().getBoundingClientRect().height - padding
+
+  const x = d3.scaleTime().range([padding, width])
+  const y0 = d3.scaleLinear().range([height, padding])
+  const y1 = d3.scaleLinear().range([height, padding])
+
+  const priceLine = d3.line()
+    .x(d => x(d.date))
+    .y(d => y0(d.price))
+
+  const priceArea = d3.area()
+    .x(d => x(d.date))
+    .y0(y0(0))
+    .y1(d => y0(d.price))
+
+  const interestLine = d3.line()
+    .x(d => x(d.date))
+    .y(d => y1(d.interest))
+
+  const interestArea = d3.area()
+    .x(d => x(d.date))
+    .y0(y1(0))
+    .y1(d => y1(d.interest))
+
   // parse dates
-  points = points.map(point => Object.assign({}, point, {
-    date: new Date(point.date)
-  }))
+  points = points
+    .map(point => Object.assign({}, point, {
+      date: new Date(point.date)
+    }))
 
   x.domain(d3.extent(points, d => d.date))
-  y0.domain([ 0, d3.max(points, d => d.price) ])
-  y1.domain([ 0, d3.max(points, d => d.interest) ])
+  y0.domain([ d3.min(points, d => d.price), d3.max(points, d => d.price) ])
+  y1.domain([ d3.min(points, d => d.interest), d3.max(points, d => d.interest) ])
 
-  svg.append('path')
-    .data([ points ])
-    .attr('d', interestLine)
-    .attr('stroke', 'rgba(0,0,0,0.2)')
-    .attr('stroke-width', '3')
-    .attr('fill', 'rgba(0,0,0,0.2)')
+  interest()
+  price()
 
-  svg.append('path')
-    .data([ points ])
-    .attr('d', priceLine)
-    .attr('stroke', '#03b0ec')
-    .attr('stroke-width', '3px')
-    .attr('stroke-opacity', '0.4')
-    .attr('fill', '#03b0ec')
-    .attr('fill-opacity', '0.3')
-    .attr('opacity', '0.9')
+  function price() {
+    svg.append('path')
+      .data([ points ])
+      .attr('d', priceArea)
+      .attr('class', 'line__area line__area--price')
 
+    svg.append('path')
+      .data([ points ])
+      .attr('d', priceLine)
+      .attr('class', 'line line--price')
+  }
+
+  function interest() {
+    svg.append('path')
+      .data([ points ])
+      .attr('d', interestArea)
+      .attr('class', 'line__area line__area--interest')
+
+    svg.append('path')
+      .data([ points ])
+      .attr('d', interestLine)
+      .attr('class', 'line line--interest')
+  }
 }
