b74f32ce1f800ac1a437def57ac4cfff423df225
nickbreaton <nick@breaton.com>
2018-02-24 16:11:37 -0500

    Add menu

---
+++ src/components/Menu.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Menu = () => (
+  <aside className='menu menu__background'>
+    hello
+  </aside>
+)
+
+export default Menu
--- src/index.js
+++ src/index.js
@@ -4,10 +4,8 @@ import { registerCache } from './lib/cache'
 import { render } from 'react-dom'
 import { store } from './state'
 import * as d3 from 'd3'
-import Graph from './components/Graph'
+import Menu from './components/Menu'
 import React from 'react'
-
-registerCache()
 
 const start = new Date()
 start.setFullYear(start.getFullYear() - 1)
@@ -20,9 +18,10 @@ store.dispatch(app.actions.pointsRequest(
 ))
 
 const root = d3.select('#root')
-  .style('padding-top', '2rem')
-  .style('display', 'flex')
-  .style('align-items', 'center')
-  .style('justify-content', 'center')
+  // .style('padding-top', '2rem')
+  // .style('display', 'flex')
+  // .style('align-items', 'center')
+  // .style('justify-content', 'center')
 
-render(<Graph />, root.node())
+render(<Menu />, root.node())
+registerCache()
